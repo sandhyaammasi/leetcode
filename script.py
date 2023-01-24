98730d7605d47959812333b7094ddac42efb2baa
Sandhya Ammasi <sandhyaammasi@gmail.com>
2023-01-23 23:46:27 -0600

    Evaluate Reverse Polish Notation 2nd solution

--- script.py
+++ script.py
@@ -1,10 +1,3 @@
-import socket
-import os
-s=socket.socket()
-#host=socket.gethostname() #server hostname
-host='172.17.8.236'
-port=12000 #same as server
-s.connect((host,port))
-fileToSend = open("ToSend.txt","r")
-content = fileToSend.read()
-s.send(content.encode())
+tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+print(tokens.pop(),tokens.pop())
+print(type(tokens.pop()))
